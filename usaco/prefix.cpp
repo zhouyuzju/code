18d03118b7ec9cdfb23b161384eeb6b08f786d62
zhouyuzju <jwjzy1020@gmail.com>
2013-01-22 21:22:04 +0800

    prefix

---
+++ usaco/prefix.cpp
@@ -0,0 +1,47 @@
+/*
+ID: jwjzy10201
+LANG: C++
+TASK: prefix
+*/
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+ofstream fout ("prefix.out");
+ifstream fin ("prefix.in");
+long len,maxn = 0,lent,dl[201];
+char pi[201][11];
+string core = "",temp;
+bool f = false;
+int main(){
+	for(len = 0;;len++){
+		fin >> pi[len];
+		if(pi[len][0] == '.')
+			break;
+		dl[len] = strlen(pi[len]);
+	}
+	len--;
+	while(fin >> temp)
+		core += temp;
+	lent = core.length();
+	for(int i = 0;i < lent;i++){
+		for(int j = 0;j <= len;j++)
+			if(i + dl[j] > lent)
+				continue;
+			else{
+				f = false; 
+				for(int k = 0;k < dl[j];k++)
+					if(core[i + k] != pi[j][k]){
+						f = true;
+						break;
+					}
+				if (!f) 
+					maxn = (dl[j] + i > maxn ? dl[j] + i : maxn);
+			}
+		if(i + 1 > maxn)
+			break;
+	}
+	fout<< maxn << endl;
+	return 0;
+}
+
